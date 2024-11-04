4c89a0c08d0171a10ea055e97b95eae76a8923e8
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-11-04 14:15:37 +0500

    added string data type

--- object/object.go
+++ object/object.go
@@ -16,6 +16,7 @@ const (
 
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
+	STRING_OBJ  = "STRING"
 
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 
@@ -84,3 +85,10 @@ func (f *Function) Inspect() string {
 
 	return out.String()
 }
+
+type String struct {
+	Value string
+}
+
+func (s *String) Type() ObjectType { return STRING_OBJ }
+func (s *String) Inspect() string  { return s.Value }
